980b78bfb49ab0d55f12b43b8d59f681dc3de101
BigSlimezy <hubblec23@hotmail.com>
2025-09-06 09:49:22 +1000

    update bookbot

---
+++ main.py
@@ -0,0 +1,11 @@
+def get_book_text(filepath):
+    with open(filepath) as f:
+        text = f.read()
+    return text
+
+def main():
+    filepath = "bookbot/books/frankenstein.txt"
+    text = get_book_text(filepath)
+    print(text)
+
+main()
